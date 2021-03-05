23fa2d3d2fc1588914fceaadf4e1d8e4149116fe
shencodes95 <alan951109@gmail.com>
2021-03-04 23:20:36 -0500

    March 4th update after class discussion

--- HW1/HW1readme.md
+++ HW1/HW1readme.md
@@ -5,4 +5,9 @@ This Readme file is prepared to explain how to run the python file called 'HW1co
 You can find the dataset required for the python file in this repository as well. 
 
 To run the python file correctly find the experiment number you want to test out and by selecting everything and use the Ctrl 
-+ / you can uncomment my code. I used three methods for each experiment. 
+ / you can uncomment my code. I used three methods for each experiment. 
+ 
+ 
+ Update after class on March 4th.
+ The dummy variables created were 1 level short. It should be based on N level because the prediction model we are running 
+ does not have an intercept. New version of the HW1code fixed that expression and updated performance measures. 
--- HW1/code.py
+++ HW1/code.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 pd.set_option('display.max_columns', 500)
@@ -24,30 +26,31 @@ dataset.fillna({"reviews_per_month":0}, inplace=True)
 #subsetting the dataset with 10 variables and ID
 dataset = dataset[[ "price", "room_type", "neighbourhood_group", "neighbourhood", "minimum_nights", "number_of_reviews", "reviews_per_month", "calculated_host_listings_count"]]
 
-print(dataset.isnull().sum())
+#print(dataset.isnull().sum())
 
-print(dataset["neighbourhood_group"].value_counts())
+#print(dataset["neighbourhood_group"].value_counts())
 
 #Here I tested price and minimum_nights variables to find outliers
-dataset["price"].plot.hist(bins=10)
-print(dataset["price"].describe())
+#dataset["price"].plot.hist(bins=10)
+#print(dataset["price"].describe())
 #x = dataset["price"].hist()
 #plt.show()
 #deciding cutoff point
 print(dataset[dataset["price"]>300])
 #subsetting and cutoff prices over 275.
 dataset = dataset[dataset["price"]<300]
-
+#subsetting and cutoff minimum nights over 14 days
 dataset["minimum_nights"].plot.hist(bins=10)
 print(dataset["minimum_nights"].describe())
 dataset = dataset[dataset["minimum_nights"]<14]
 
 #experiment 1
-#create k-1 dummy to prevent autocorrelation
-dataset_1 = pd.get_dummies(dataset, columns=["neighbourhood_group","room_type"], prefix = ["nbhdg","rt"],drop_first=True)
-dataset_1.drop(["neighbourhood"], axis =1, inplace= True)
-data = dataset_1.loc[:, dataset_1.columns != "price"]
-target = dataset_1["price"]
+# #create k-1 dummy to prevent autocorrelation
+# dataset_1 = pd.get_dummies(dataset, columns=["neighbourhood_group","room_type"], prefix = ["nbhdg","rt"])
+# #remove drop_first = TRUE argument so that we have N number of dummies for N level
+# dataset_1.drop(["neighbourhood"], axis =1, inplace= True)
+# data = dataset_1.loc[:, dataset_1.columns != "price"]
+# target = dataset_1["price"]
 
 # regular regression
 # x_train1, x_test1, y_train1, y_test1 = train_test_split(data, target, test_size=0.20, random_state=42)
@@ -64,28 +67,31 @@ target = dataset_1["price"]
 # print(list(data.columns))
 # print(1-(1-machine.score(x_train1,y_train1))*(len(y_train1)-1)/(len(y_train1) - x_train1.shape[1]-1))
 #use lasso
-x_train1, x_test1, y_train1, y_test1 = train_test_split(data, target, test_size=0.20, random_state=42)
-machine = linear_model.Lasso(alpha = 0.01, normalize = True).fit(x_train1, y_train1)
-print(machine.score(x_train1, y_train1))
-print(machine.coef_)
-print(list(data.columns))
-print(1-(1-machine.score(x_train1,y_train1))*(len(y_train1)-1)/(len(y_train1) - x_train1.shape[1]-1))
+# x_train1, x_test1, y_train1, y_test1 = train_test_split(data, target, test_size=0.20, random_state=42)
+# machine = linear_model.Lasso(alpha = 0.01, normalize = True).fit(x_train1, y_train1)
+# print(machine.score(x_train1, y_train1))
+# print(machine.coef_)
+# print(list(data.columns))
+# print(1-(1-machine.score(x_train1,y_train1))*(len(y_train1)-1)/(len(y_train1) - x_train1.shape[1]-1))
 
 #Experiment 2
 
-# dataset_1 = pd.get_dummies(dataset, columns=["room_type", "neighbourhood"], prefix = ["rt","nbhd"],drop_first=True)
-# dataset_1.drop(["neighbourhood_group"], axis =1, inplace= True)
-# data = dataset_1.loc[:, dataset_1.columns != "price"]
-# target = dataset_1["price"]
-# print(list(data.columns))
-#
-# # regular regression
-# x_train1, x_test1, y_train1, y_test1 = train_test_split(data, target, test_size=0.20, random_state=42)
-# machine = linear_model.LinearRegression().fit(x_train1, y_train1)
-# print(machine.score(x_train1, y_train1))
-# print(machine.coef_)
-# print(1-(1-machine.score(x_train1,y_train1))*(len(y_train1)-1)/(len(y_train1) - x_train1.shape[1]-1))
-#use ridge
+dataset_1 = pd.get_dummies(dataset, columns=["room_type", "neighbourhood"], prefix = ["rt","nbhd"])
+dataset_1.drop(["neighbourhood_group"], axis =1, inplace= True)
+data = dataset_1.loc[:, dataset_1.columns != "price"]
+target = dataset_1["price"]
+print(list(data.columns))
+
+# regular regression
+x_train1, x_test1, y_train1, y_test1 = train_test_split(data, target, test_size=0.20, random_state=42)
+machine = linear_model.LinearRegression().fit(x_train1, y_train1)
+print(machine.score(x_train1, y_train1))
+print(machine.coef_)
+print(1-(1-machine.score(x_train1,y_train1))*(len(y_train1)-1)/(len(y_train1) - x_train1.shape[1]-1))
+predictions = machine.predict(x_test1)
+print(predictions)
+
+# # use ridge
 # x_train1, x_test1, y_train1, y_test1 = train_test_split(data, target, test_size=0.20, random_state=42)
 # machine = linear_model.Ridge(alpha = 0.01, normalize = True).fit(x_train1, y_train1)
 # print(machine.score(x_train1, y_train1))
